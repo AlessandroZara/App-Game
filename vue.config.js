fcaff09688c5843523796fc6cfe6a8e340f1ba02
AlessandroZara <alezara@hotmail.it>
2022-02-18 18:45:13 +0100

    delete vue config

--- vue.config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-   publicPath: process.env.NODE_ENV === 'production' ? '/App1/' : '/',
-   outputDir: 'C:/Users//aleza//Desktop/Corso Javascript 2021/Esercitazioni/EsercitazioneMie/APP SCRITTE IN VUE/App-Game'
-  }
